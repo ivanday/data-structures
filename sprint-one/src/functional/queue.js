c5106ea2377b4ca55a5179c1b8259477b45819ae
Ivan Day <ivanday@utexas.edu>
2022-10-26 18:09:02 -0500

    Create functional implementations of Queue and Stack and pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,34 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (size === 0) {
+      size = size + 1;
+      storage[size] = value;
+    } else {
+      for (var i = size; i > 0; i--) {
+        storage[i + 1] = storage[i];
+      }
+      storage[1] = value;
+      size = size + 1;
+    }
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      var temp = storage[size];
+      delete storage[size];
+      size = size - 1;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    console.log(size);
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size = size + 1;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    if (size !== 0) {
+      let temp = storage[size];
+      delete storage[size];
+      size = size - 1;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
