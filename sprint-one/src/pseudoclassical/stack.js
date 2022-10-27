adc06b21f28aa4a57325bddbf6fd0734daa3f558
Ivan Day <ivanday@utexas.edu>
2022-10-27 10:37:33 -0500

    Write pseudoclassical Stack and pass all tests

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.objSize = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.objSize += 1;
+  this.storage[this.objSize] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.objSize !== 0) {
+    var temp = this.storage[this.objSize];
+    delete this.storage[this.objSize];
+    this.objSize -= 1;
+    return temp;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.objSize;
+};
 
