ccf5c873a60eef169c68c26016d534be50242bd0
Ivan Day <ivanday@utexas.edu>
2022-10-27 16:40:33 -0500

    Implement insert method of Binary Search Tree and begin contains method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,71 @@
 var BinarySearchTree = function(value) {
+  var newBinarySearchTree = {};
+
+  newBinarySearchTree.value = value;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  _.extend(newBinarySearchTree, binarySearchTreeMethods);
+
+  return newBinarySearchTree;
 };
 
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(newValue) {
+  //check if value given as argument is greater than or less than the current objects value
+  var newTree = BinarySearchTree(newValue);
+
+  if (newValue > this.value) {
+    if (this.right === null) {
+      console.log(newValue);
+      this.right = newTree;
+    } else {
+      this.right.insert(newValue);
+    }
+  } else if (newValue < this.value) {
+    if (this.left === null) {
+      console.log(newValue);
+      this.left = newTree;
+    } else {
+      this.left.insert(newValue);
+    }
+  }
+  //check if the appropriate side child is null
+  //if it is, create new tree with value, set the child to the new tree
+  //if not, recursively call insert with the appropriate child
+
+};
+
+binarySearchTreeMethods.contains = function(target) {
+  // Check if current tree have is our target value
+  console.log('current tree value: ' + this.value);
+  if (this.value === target ) {
+    // if so ... return true
+    return true;
+  }
+  //check if target value is less than or greater than current tree value
+  if (target > this.value) {
+    //check if appropriate child (left or right) is null
+    if (this.right === null) {
+      //if it is, return false
+      return false;
+    } else {
+      //else run recursion on that child
+      this.right.contains(target);
+    }
+  } else if (target < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      this.left.contains(target);
+    }
+  }
+};
+
+binarySearchTreeMethods.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
