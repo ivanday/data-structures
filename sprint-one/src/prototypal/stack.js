8c64c530d32acd75c75df1c82e83db0e981cceb3
Ivan Day <ivanday@utexas.edu>
2022-10-27 09:26:28 -0500

    Write prototypal stack and pass all tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(stackMethods);
+  newInstance.storage = {};
+  newInstance.objSize = 0;
+  return newInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  'push': function(value) {
+    this.objSize += 1;
+    this.storage[this.objSize] = value;
+  },
+  'pop': function() {
+    if (this.objSize !== 0) {
+      var temp = this.storage[this.objSize];
+      delete this.storage[this.objSize];
+      this.objSize -= 1;
+      return temp;
+    }
+  },
+  'size': function() {
+    return this.objSize;
+  }
+};
 
 
