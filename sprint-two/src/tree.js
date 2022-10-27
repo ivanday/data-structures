3674ae21c47015f8603aad7660aa52ff920ceb1e
Ivan Day <ivanday@utexas.edu>
2022-10-27 15:48:56 -0500

    Create Tree class and methods and pass all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = []; // fix me
 
   return newTree;
 };
@@ -11,9 +13,30 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  //base case: check if the value is equal to target
+  if (this.value === target) {
+    return true;
+  }
+  //check if there are children, if there are, iterate of children anc call contains on each one
+  if (this.children.length > 0) {
+    // this.children.forEach(function(child) {
+    //   child.contains(target);
+    // });
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+
+  }
+
+  //return false
+  return false;
 };
 
 
