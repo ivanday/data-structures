6f7febbcbcda563b57e2830078efff57fb6744d9
Ivan Day <ivanday@utexas.edu>
2022-10-27 10:42:41 -0500

    Write es6 queue and pass all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.objSize = 0;
+  }
+
+  enqueue(value) {
+    if (this.objSize === 0) {
+      this.objSize += 1;
+      this.storage[this.objSize] = value;
+    } else {
+      for (var i = this.objSize; i > 0; i--) {
+        this.storage[i + 1] = this.storage[i];
+      }
+      this.storage[1] = value;
+      this.objSize += 1;
+    }
+  }
+
+  dequeue() {
+    if (this.objSize !== 0) {
+      var temp = this.storage[this.objSize];
+      delete this.storage[this.objSize];
+      this.objSize -= 1;
+      return temp;
+    }
+  }
+
+  size() {
+    return this.objSize;
   }
 
 }
