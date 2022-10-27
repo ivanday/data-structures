1dd3769cc9294c0e6fc69709eebae32349e53cc7
Ivan Day <ivanday@utexas.edu>
2022-10-27 09:03:07 -0500

    Write functional shared instantiation of queue and pass all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {'storage': {}, 'objSize': 0};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  'enqueue': function(value) {
+    if (this.objSize === 0) {
+      this.objSize = this.objSize + 1;
+      this.storage[this.objSize] = value;
+    } else {
+      for (var i = this.objSize; i > 0; i--) {
+        this.storage[i + 1] = this.storage[i];
+      }
+      this.storage[1] = value;
+      this.objSize = this.objSize + 1;
+    }
+  },
+  'dequeue': function() {
+    if (this.objSize > 0) {
+      var temp = this.storage[this.objSize];
+      delete this.storage[this.objSize];
+      this.objSize = this.objSize - 1;
+      return temp;
+    }
+  },
+  'size': function() {
+    return this.objSize;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,8 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var newInstance = {'objSize': 0, 'storage': {}};
-  //var storage = {};
-  //var objSize = 0;
+
   _.extend(newInstance, stackMethods);
   return newInstance;
 };
