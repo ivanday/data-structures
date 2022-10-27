786b67dc2ba64ea02bfe4155fe245e510c699707
Ivan Day <ivanday@utexas.edu>
2022-10-27 10:45:12 -0500

    Write es6 Stack and pass all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.objSize = 0;
   }
 
+  push(value) {
+    this.objSize += 1;
+    this.storage[this.objSize] = value;
+  }
+
+  pop() {
+    if (this.objSize !== 0) {
+      var temp = this.storage[this.objSize];
+      delete this.storage[this.objSize];
+      this.objSize -= 1;
+      return temp;
+    }
+  }
+
+  size() {
+    return this.objSize;
+  }
 }
