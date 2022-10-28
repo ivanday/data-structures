9f00995b9339ed77ac85d78f92cb3e2966e71476
Ivan Day <ivanday@utexas.edu>
2022-10-28 14:51:53 -0500

    Implement set data structure and pass all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
+
 };
 
 setPrototype.remove = function(item) {
+  var indexof = this._storage.indexOf(item);
+  if (indexof > -1) {
+    this._storage.splice(indexof, 1);
+  }
 };
 
 /*
